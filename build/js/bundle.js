0259ca3e850f77ff19f833b45285fe2ec10ca393
JKCamus <924850758@qq.com>
2021-04-11 20:43:12 +0800

    update

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,47 +1,65 @@
-// 定义了一个对象
-// 模块的路径(key): 函数(value)
-var __webpack_modules__ = {
-  "./src/js/format.js":
-    (function (module) {
-      const dateFormat = (date) => {
-        return "2021";
-      }
-      const priceFormat = (price) => {
-        return "100.00";
-      }
-      // 将我们要导出的变量, 放入到module对象中的exports对象
-      module.exports = {
-        dateFormat,
-        priceFormat
-      }
-    })
-}
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
 
-// 定义一个对象, 作为加载模块的缓存
-var __webpack_module_cache__ = {};
+/***/ "./src/js/format.js":
+/*!**************************!*\
+  !*** ./src/js/format.js ***!
+  \**************************/
+/***/ (function(module) {
 
-// 是一个函数, 当我们加载一个模块时, 都会通过这个函数来加载
-function __webpack_require__(moduleId) {
-  // 1.判断缓存中是否已经加载过
-  if (__webpack_module_cache__[moduleId]) {
-    return __webpack_module_cache__[moduleId].exports;
-  }
+const dateFormat = date => {
+  return "2021";
+};
 
-  // 2.给module变量和__webpack_module_cache__[moduleId]赋值了同一个对象
-  var module = __webpack_module_cache__[moduleId] = { exports: {} };
+const priceFormat = price => {
+  return "100.00";
+};
 
-  // 3.加载执行模块
-  __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+module.exports = {
+  dateFormat,
+  priceFormat
+};
 
-  // 4.导出module.exports {dateFormat: function, priceForamt: function}
-  return module.exports;
-}
+/***/ })
 
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+!function() {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const {
+  dateFormat,
+  priceFormat
+} = __webpack_require__(/*! ./js/format */ "./src/js/format.js");
 
-// 具体开始执行代码逻辑
-!function () {
-  // 1.加载./src/js/format.js
-  const { dateFormat, priceFormat } = __webpack_require__("./src/js/format.js");
-  console.log(dateFormat("hello"));
-  console.log(priceFormat("hello"));
+console.log(dateFormat("hello"));
+console.log(priceFormat("hello"));
 }();
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
